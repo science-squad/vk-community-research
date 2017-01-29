1037ab636bff3148aacd9f1d42a79ad19a74566c
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-29 21:49:15 +0300

    media and static roots setup

--- backend/vkcr/settings.py
+++ backend/vkcr/settings.py
@@ -124,6 +124,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 VK_COMMUNITY_START_ID = 1
