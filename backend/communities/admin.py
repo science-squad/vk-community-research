3af92aa48014e6868d67b8bd206eda9d20377cf5
Grigoriy Beziuk <gbezyuk@gmail.com>
2017-01-29 20:41:59 +0300

    created + updated timestamps; vk request task model

--- backend/communities/admin.py
+++ backend/communities/admin.py
@@ -1,14 +1,21 @@
 from django.contrib import admin
-from .models import VkCommunity, VkPerson
+from .models import VkCommunity, VkPerson, VkRequestTask
 
 @admin.register(VkCommunity)
 class VkCommunityAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__', 'vk_id', 'estimated_members_count', 'member_list_length', 'is_valid', 'created_at', 'updated_at')
     readonly_fields = ('member_list_length', 'vk_id', 'estimated_members_count', 'member_list_length', 'is_valid', 'member_list')
-    list_display = ('id', '__str__', 'vk_id', 'estimated_members_count', 'member_list_length', 'is_valid')
 
 
 @admin.register(VkPerson)
 class VkPersonAdmin(admin.ModelAdmin):
-    list_display = ('id', '__str__', 'vk_id', 'first_name', 'last_name', 'sex')
+    list_display = ('id', '__str__', 'vk_id', 'first_name', 'last_name', 'sex', 'created_at', 'updated_at')
     readonly_fields = ('vk_id', 'first_name', 'last_name', 'sex')
     list_filter = ('sex',)
+
+
+@admin.register(VkRequestTask)
+class VkRequestTaskAdmin(admin.ModelAdmin):
+    list_display = ('id', '__str__', 'start_vk_id', 'stop_vk_id', 'type', 'created_at', 'updated_at')
+    readonly_fields = ('start_vk_id', 'stop_vk_id', 'type')
+    list_filter = ('type',)
--- backend/communities/models.py
+++ backend/communities/models.py
@@ -13,6 +13,9 @@ class VkCommunity(models.Model):
     member_list = models.TextField(blank=True, null=True)
     member_list_length = models.PositiveIntegerField(verbose_name=_('loaded member list length'), null=True)
 
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
     def __str__(self):
         return _("Group #%d") % self.vk_id
 
@@ -31,3 +34,29 @@ class VkPerson(models.Model):
     first_name = models.CharField(verbose_name=_('first name'), max_length=255, blank=True, null=True)
     last_name = models.CharField(verbose_name=_('last name'), max_length=255, blank=True, null=True)
     sex = models.IntegerField(verbose_name=_('sex'), null=True)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+
+VK_REQUEST_TASK_TYPE_CHOICES = (
+    ('ecmc', _('estimate communities member count')),
+    ('rcml', _('request communities member list')),
+)
+
+
+class VkRequestTask(models.Model):
+
+    class Meta:
+        verbose_name = _('vk request task')
+        verbose_name_plural = _('vk request task')
+
+    start_vk_id = models.PositiveIntegerField(verbose_name=_('start vk id'))
+    stop_vk_id = models.PositiveIntegerField(verbose_name=_('stop vk id'))
+    type = models.CharField(choices=VK_REQUEST_TASK_TYPE_CHOICES, max_length=10)
+
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return _("VK Request Task %(type)s #%(id)d") % {'type': self.type, 'id': self.id }
